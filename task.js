cae9c92c4893ff8818825893e22b97753199fe11
Banachos <gosha.romanovich.2016@mail.ru>
2023-07-10 21:55:44 +0300

    task3

--- task.js
+++ task.js
@@ -44,3 +44,13 @@ function averageEvenElementsWorker(...arr){
 }
 // TASK - 3
 // ---------------------------------------------------------------------------
+function makeWork(arrOfArr, func) {
+    let maxWorkerResult = -Infinity;
+    for (let i = 0; i < arrOfArr.length; i++) {
+    const result = func(...arrOfArr[i]);
+    if (result > maxWorkerResult) {
+        maxWorkerResult = result;
+    }
+}
+    return maxWorkerResult;
+}
