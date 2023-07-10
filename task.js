682b457dc53eb97f94fe7877b12909f0916405ed
Banachos <gosha.romanovich.2016@mail.ru>
2023-07-10 21:21:01 +0300

    task2

--- task.js
+++ task.js
@@ -9,3 +9,38 @@ function getArrayParams(...arr){
 }
 // TASK - 2
 // ----------------------------------------------------------------------
+function summElementsWorker(...arr){
+    let sum = arr.reduce((acc,elem) => acc + elem)
+    return sum
+}
+let differenceMaxMinWorker = (...arr) => Math.max(...arr) - Math.min(...arr)
+function differenceEvenOddWorker(...arr){
+    let sumEvenElement = 0
+    let sumOddElement = 0
+for (let i = 0; i < arr.length; i++) {
+    if (arr[i] % 2 === 0) {
+        sumEvenElement += arr[i]
+    } else {
+        sumOddElement += arr[i]
+    }
+}
+if (sumOddElement === 0) {
+    return sumEvenElement;
+} else {
+    return sumEvenElement - sumOddElement;
+}
+}
+
+function averageEvenElementsWorker(...arr){
+    let sumEvenElement = 0
+    let countEvenElement = 0
+    for (let i = 0; i < arr.length; i++) {
+        if(arr[i] % 2 === 0){
+            sumEvenElement += arr[i]
+            countEvenElement++
+        }
+    }
+    return sumEvenElement / countEvenElement
+}
+// TASK - 3
+// ---------------------------------------------------------------------------
